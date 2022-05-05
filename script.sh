28005fe40dcf60d02c5aba219f1713ad37d8e887
carlos ponce <cadu.lp@gmail.com>
2022-05-05 16:41:07 -0300

    revisão

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # Este script pode ser usado para:
 # Fazer backup, compactar e enviar para o S3(AWS)
-# Ex.: ./script.sh backup <nome_do_banco_dados>
+# Ex.: ./script.sh backup <name_db>
 # Listar os arquivos no S3(AWS)
 # Ex.: ./script.sh list
 # Fazer download do arquivo do S3, descomactar e fazer o restore no banco de dados.
@@ -20,13 +20,13 @@ DATABASE_NAMES="$2"
 
 backup() {
     echo "Executando backup para bancos de dados selecionado ${DATABASE_NAMES}"
-    mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --db ${DATABASE_NAMES} --out dumps/kitnet-${NAME_BACKUP}/
-    mv dumps/kitnet-${NAME_BACKUP}/kitnet/* dumps/kitnet-${NAME_BACKUP}
+    mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --db ${DATABASE_NAMES} --out dumps/<name_db>-${NAME_BACKUP}/
+    mv dumps/<name_db>-${NAME_BACKUP}/<name_db>/* dumps/<name_db>-${NAME_BACKUP}
     rmdir dumps/kitnet-${NAME_BACKUP}/kitnet
 }
 
 compress(){
-    tar -cvzf database.tar.gz  dumps/kitnet-${NAME_BACKUP}* 
+    tar -cvzf database.tar.gz  dumps/<name_db>-${NAME_BACKUP}* 
     cp -r database.tar.gz ${NAME_ARQ}.tar.gz
     rm -r database.tar.gz dumps
     
@@ -40,18 +40,18 @@ unzip(){
 
 upload_s3(){
     # envia backup para o S3 AWS - Não esquecer de: export AWS_PROFILE=UserProfile(AWS)
-    aws s3 cp ${NAME_ARQ}.tar.gz s3://bcpmongoragazzi/database/ --include ${NAME_ARQ}.tar.gz 
+    aws s3 cp ${NAME_ARQ}.tar.gz s3://<name_s3> --include ${NAME_ARQ}.tar.gz 
 }
 
 
 download_s3(){
     # recebe backup do S3 AWS - Não esqucer de: export AWS_PROFILE=UserProfile(AWS)
-    aws s3 cp s3://bcpmongoragazzi/database/${NAME_REST} ./  
+    aws s3 cp s3://<name_s3>/${NAME_REST} ./  
 }
 
 list_s3(){
     # Lista arquivos do S3 (AWS)0
-    aws s3 ls s3://bcpmongoragazzi/database/ > info.txt
+    aws s3 ls s3://<name_s3>/ > info.txt
     cut -d' ' -f9 info.txt
     rm -r info.txt
     
@@ -61,7 +61,7 @@ case $1 in
   backup)
     backup "$2"
     compress
-    # upload_s3
+    upload_s3
     ;;
 
   list)
@@ -69,8 +69,8 @@ case $1 in
     ;;
 
   restore)
-    # download_s3
-    # sleep 5
+    download_s3
+    sleep 5
     unzip "$2"
     ;;
 
